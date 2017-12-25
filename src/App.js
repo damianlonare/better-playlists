d9bdfc0f3dad159a9e8e29c44d2a3e1ba2ad97aa
Damian Arechar <damian.lon.are@gmail.com>
2017-12-25 16:04:09 -0600

    Initial Commit
    Copied everything from the previous episode.

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let defaultTextColor = 'grey';
+let defaultStyle = {
+  color: defaultTextColor
+};
+
+class Aggregate extends Component {
+  render () {
+    return (
+      <div style={{...defaultStyle, width: '25%', display: 'inline-block'}}>
+        <h3 style={{display: 'inline-block'}}>Number Text</h3>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render () {
+    return (
+      <div>
+        <img/>
+        <input type=""/>
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div style={{...defaultStyle, width: "25%", display: "inline-block"}}>
+        <img/>
+        <h3>Playlist Name</h3>
+        <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 style={defaultStyle}>Title</h1>
+        <Aggregate/>
+        <Aggregate/>
+        <Filter/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
+      </div>
+    );
+  }
+}
+
+export default App;
